747fd1094178c8242f97d7d688ce491eac6a2dae
suji <suji.ravuri@gmail.com>
2024-01-25 12:01:40 -0500

    Commited the changes done to requirement x

---
+++ src/com/company/add.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class add {
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2= 2;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
